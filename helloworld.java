e07d202df022025e53f52f478f777455c45e9a8e
Vibha Bhavikatti <g00089208@aus.edu>
2023-01-31 17:53:55 +0400

    Feature B added

--- helloworld.java
+++ helloworld.java
@@ -3,8 +3,11 @@ package helloworld;
 public class helloworld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello World");
+	}
+	public void FeatureB(String s)
+	{
+		System.out.println(s);
 	}
 
 }
